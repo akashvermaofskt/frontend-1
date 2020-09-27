c1b7fe4e0d1550f90c67a7224f71f49a4c6f555e
Akash Verma <akash.verma@lidolearning.com>
2020-09-27 17:37:13 +0530

    Minor change#6

--- src/App.js
+++ src/App.js
@@ -15,13 +15,13 @@ class App extends Component {
     return (
       <Router>
         <div className="container">
-          
+
           <Navbar></Navbar>
 
           <Route path='/' exact component={TodosList} />
-          <Route path='/edit/:id' component={EditTodo} /> 
-          <Route path='/create' component={CreateTodo} /> 
-        <Route path='/delete/:id' component={DeleteTodo}></Route>
+          <Route path='/edit/:id' component={EditTodo} />
+          <Route path='/create' component={CreateTodo} />
+          <Route path='/delete/:id' component={DeleteTodo}></Route>
         </div>
       </Router>
     );
